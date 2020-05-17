4025ab23e12bd8a5f8afd959ec207897423d9674
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2020-05-17 01:55:26 -0500

    Starter Files

---
+++ student_code.py
@@ -0,0 +1,12 @@
+import common
+
+def drone_flight_planner (map,policies, values, delivery_fee, battery_drop_cost, dronerepair_cost, discount_per_cycle):
+	# PUT YOUR CODE HERE
+	# access the map using "map[y][x]"
+	# access the policies using "policies[y][x]"
+	# access the values using "values[y][x]"
+	# y between 0 and 5
+	# x between 0 and 5
+	# function must return the value of the cell corresponding to the starting position of the drone
+	# 
+	return 100000000
